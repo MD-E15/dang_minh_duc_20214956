e9daea2b7c6c42343e51c1e04486e5c20caccadc
Đặng Minh Đức <114754286+MD-E15@users.noreply.github.com>
2023-04-14 14:17:30 +0700

    Add files via upload

---
+++ lab01/InputFromKeyboard.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class InputFromKeyboard {
+    public static void main(String args[]){
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("what's is your name?");
+        String name = keyboard.nextLine();
+        System.out.println("How old are you?");
+        int iAge = keyboard.nextInt();
+        System.out.println("How tall are you (m) ?");
+        double dHeight = keyboard.nextDouble();
+
+        System.out.println("Mrs/Ms. " + name + "," + iAge +" years old. " + "Your height is " +dHeight + ".");
+    }
+}
